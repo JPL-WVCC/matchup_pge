0abacc2e7d0dd126b98fe7a7cebab61635a78bd9
leipan <leipan@jpl.nasa.gov>
2020-06-21 17:21:31 -0700

    developped cris_viirs_cfg()

--- util/wvcc_evaluator.py
+++ util/wvcc_evaluator.py
@@ -4,6 +4,7 @@ from builtins import str
 import os
 import sys
 import time
+import datetime
 import json
 import requests
 import logging
@@ -63,7 +64,7 @@ def query_es(query, idx, url=app.conf['GRQ_ES_URL']):
     query_url = "{}/{}/_search?search_type=scan&scroll=60&size=100".format(url, idx)
     logger.info("url: {}".format(url))
     logger.info("idx: {}".format(idx))
-    logger.info("query: {}".format(json.dumps(query, indent=2)))
+    ### logger.info("query: {}".format(json.dumps(query, indent=2)))
     r = requests.post(query_url, data=json.dumps(query))
     r.raise_for_status()
     scan_result = r.json()
@@ -77,6 +78,20 @@ def query_es(query, idx, url=app.conf['GRQ_ES_URL']):
         if len(res['hits']['hits']) == 0: break
         hits.extend(res['hits']['hits'])
     return hits
+
+
+def query_range(dataset_type, str_start_time, str_end_time):
+  query = {"query":{
+             "bool":{
+               "must":[{"term":{"dataset_type.raw":dataset_type}}, {"range":{"starttime":{"gte":str_start_time}}}, {"range":{"endtime":{"lte":str_end_time}}}]
+             }
+           },
+           "sort":[{"_timestamp":{"order":"desc"}}],
+           "fields":["_timestamp","_source"]
+          }
+  result = query_es(query, '')
+
+  return result
 
 
 def resolve_acq(slc_id, version):
@@ -125,26 +140,81 @@ def cris_viirs_cfg(start_time, end_time):
   # plus one additional VIIRS granule on either side of the day boundary
   # to form cris_viirs_cfg, and publish it to GRQ
 
-  # get all CrIS granules between the dates
+  config_cris_viirs = {}
+
+
+  # --- sounder CrIS
+
+  str_start_time = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+  logger.info("str_start_time: {}".format(str_start_time))
+  str_end_time = end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+  logger.info("str_end_time: {}".format(str_end_time))
+
+  # get all CrIS granules between start_time and end_time
   dataset_type = 'CRIS-data'
-  query = {"query":{
-             "bool":{
-               "must":[{"term":{"dataset_type.raw":dataset_type}}, {"range":{"starttime":{"gte":start_time}}}, {"range":{"endtime":{"lte":end_time}}}]
-             }
-           },
-           "sort":[{"_timestamp":{"order":"desc"}}],
-           "fields":["_timestamp","_source"]
-          }
-  result = query_es(query, '')
-  logger.info("result: %s" % result)
+  result = query_range(dataset_type, str_start_time, str_end_time)
+
+  ### logger.info("result: {}".format(json.dumps(result, indent=2)))
+  logger.info("len(result): %s" % str(len(result)))
+  logger.info("")
+
+  # collect all sounder granules in a list
+  logger.info("----------- sounder granule urls: ----------")
+  sounder_urls = query_result_2_url_list(result)
+
+  config_cris_viirs['sounder_granule_urls'] = sounder_urls
+
+
+  # --- imager VIIRS
+
+  imager_urls = []
+
+  # get all VIIRS granules between start_time and end_time
+  dataset_type = 'VNP03MOD-data'
+  result = query_range(dataset_type, str_start_time, str_end_time)
+  logger.info("")
+  ### logger.info("result: %s" % result)
+  ### logger.info("VIIRS result: {}".format(json.dumps(result, indent=2)))
+  logger.info("len(result): %s" % str(len(result)))
+  logger.info("")
+
+  # collect all imager granules in a list
+  logger.info("----------- imager granule urls: ----------")
+  vlist0 = query_result_2_url_list(result)
+
+  # get two VIIRS granules on the boundary
+  str_start_time1 = (start_time-datetime.timedelta(minutes=12)).strftime('%Y-%m-%dT%H:%M:%SZ')
+  result = query_range(dataset_type, str_start_time1, str_start_time)
+  logger.info("----------- imager granule left boundary urls: ----------")
+  vlist1 = query_result_2_url_list(result)
+
+  str_end_time1 = (end_time+datetime.timedelta(minutes=12)).strftime('%Y-%m-%dT%H:%M:%SZ')
+  result = query_range(dataset_type, str_end_time, str_end_time1)
+  logger.info("----------- imager granule right boundary urls: ----------")
+  vlist2 = query_result_2_url_list(result)
+
+  imager_urls += vlist1
+  imager_urls += vlist0
+  imager_urls += vlist2
+
+  config_cris_viirs['imager_granule_urls'] = imager_urls
+
+  logger.info("config_cris_viirs: {}".format(json.dumps(config_cris_viirs, indent=2)))
+
+
+
+  # publish to GRQ/ES
 
 
 
 
+def query_result_2_url_list(result):
+  list1 = []
+  for item in result:
+    list1.append(item['_source']['urls'])
 
-
-
-
+  logger.info("item urls: {}".format(json.dumps(list1, indent=2)))
+  return list1
 
 
 
@@ -173,9 +243,13 @@ def main():
     dataset_type = "VNP03MOD-data"
     ### test_query(dataset_type)
 
-    ### cris_viirs_cfg("2015-06-01T20:15:00.000Z", "2015-06-01T20:55:00.000Z")
+    start_time = datetime.datetime(2015, 06, 01, 20, 15, 00, 000)
+    end_time = datetime.datetime(2015, 06, 01, 20, 55, 00, 000)
 
-    cris_viirs_cfg('2015-06-01T20:45:00Z', '2015-06-01T20:55:00Z')
+    ### cris_viirs_cfg("2015-06-01T20:15:00.000Z", "2015-06-01T20:55:00.000Z")
+    cris_viirs_cfg(start_time, end_time)
+
+    ### cris_viirs_cfg('2015-06-01T10:45:00Z', '2015-06-25T20:55:00Z')
 
 if __name__ == "__main__":
     try:
