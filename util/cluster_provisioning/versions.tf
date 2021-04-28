229dfba062775e2dd7c5972b0a6e8e83969b69ab
leipan <leipan@jpl.nasa.gov>
2021-04-28 20:36:37 +0000

    changed to symlink

--- util/cluster_provisioning/versions.tf
+++ util/cluster_provisioning/versions.tf
@@ -1,14 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-  required_version = ">= 0.13"
-}
+modules/common/versions.tf
