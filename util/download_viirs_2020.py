6016a0e9020d22a06a8ff7d4678c5e9c5733fdd4
leipan <leipan@jpl.nasa.gov>
2021-11-27 12:03:11 -0800

    init

---
+++ util/download_viirs_2020.py
@@ -0,0 +1,15 @@
+import subprocess
+import shlex
+
+work_dir = '/raid15/leipan/VIIRS/'
+
+for day1 in range(213, 246):
+  cmd = 'wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5110/VNP03MOD/2020/{0}/" --header "Authorization: Bearer bGVpcGFuOmJHVnBjR0Z1UUdwd2JDNXVZWE5oTG1kdmRnPT06MTYzNTQ2NTQ3Nzo5NjA2YTczNjJmNWUyYWExNTIzMDM5OTk5MDYyOTdkNDgzYjMyMTA4" -P .'.format(day1)
+  print('cmd: ', cmd)
+  args = shlex.split(cmd)
+  proc = subprocess.Popen(cmd, shell=True, cwd=work_dir, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+  out, err = proc.communicate()
+  print('out: ', out)
+  print('err: ', err)
+  
+
